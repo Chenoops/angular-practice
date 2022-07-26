2e20a708238aee5173293244e030f7b5f2983266
chencc <chencc3051@163.com>
2022-07-26 18:39:34 +0800

    Second Commit

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap, Params } from '@angular/router';
+import { Observable, Observer} from 'rxjs';
+import { switchMap,distinctUntilChanged } from "rxjs/operators";
 
 @Component({
   selector: 'app-second',
@@ -11,15 +13,23 @@ export class SecondComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
   ) { }
+  // todos$: Observable<Params> = this.route.params.pipe()
+  todos$: Observable<Params> = this.route.params
+
+
+  time = new Observable<string>((observer: Observer<string>) => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
+
 
   name=''
   ngOnInit() {
 
+    console.log(this.route.params);
+
     this.route.params.subscribe(params => {
       console.log(params);
-
     })
 
   }
-
 }
